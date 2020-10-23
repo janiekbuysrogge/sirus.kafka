351583fcfebe662d8bcce02e0719a5f41a036eb5
Janiek Buysrogge <janiek.buysrogge@gmail.com>
2020-10-23 17:01:23 +0200

    Add project files.

---
+++ sirus.kafka.producer/Controllers/WeatherForecastController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Avro;
+using Avro.Generic;
+using Confluent.Kafka;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace sirus.kafka.producer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            await SendMessageDefault("GET weatherforecast");
+            await SendMessageAvro("GET weatherforecast");
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        private async Task SendMessageDefault(string message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "kafka:29092",
+                ClientId = Dns.GetHostName(),
+                
+            };
+
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                await producer.ProduceAsync("api-methods-requested-default", new Message<Null, string> { Value = message });
+            }
+        }
+
+        private async Task SendMessageAvro(string message)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "kafka:29092",
+                ClientId = Dns.GetHostName()
+            };
+
+            String key = "key1";
+            String userSchema = "{\"type\":\"record\"," +
+                                "\"name\":\"myrecord\"," +
+                                "\"fields\":[{\"name\":\"f1\",\"type\":\"string\"}]}";
+            var schema = (RecordSchema)RecordSchema.Parse(userSchema);
+            GenericRecord avroRecord = new GenericRecord(schema);
+            avroRecord.Add("f1", "value");
+
+            var schemaRegistryUrl = "http://schema-registry:8085";
+            using (var schemaRegistry = new CachedSchemaRegistryClient(new SchemaRegistryConfig { Url = schemaRegistryUrl }))
+            {
+                using (var producer = new ProducerBuilder<string, GenericRecord>(config)
+                    .SetKeySerializer(new AvroSerializer<string>(schemaRegistry))
+                    .SetValueSerializer(new AvroSerializer<GenericRecord>(schemaRegistry))
+                    .Build())
+                {
+                    await producer.ProduceAsync("api-methods-requested-avro",
+                        new Message<string, GenericRecord> { Key = Guid.NewGuid().ToString("N"), Value = avroRecord });
+                }
+            }
+        }
+    }
+}
---
+++ sirus.kafka/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading.Tasks;
+using Avro.Generic;
+using Confluent.Kafka;
+using Confluent.Kafka.SyncOverAsync;
+using Confluent.SchemaRegistry;
+using Confluent.SchemaRegistry.Serdes;
+
+namespace sirus.kafka.consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Listening for messages");
+
+            Task.Factory.StartNew(() =>
+            {
+                var config = new ConsumerConfig
+                {
+                    GroupId = "request-consumers",
+                    BootstrapServers = "kafka:29092",
+                    AutoOffsetReset = AutoOffsetReset.Earliest
+                };
+
+                using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    consumer.Subscribe("api-methods-requested-default");
+                    while (true)
+                    {
+                        var result = consumer.Consume();
+                        Console.WriteLine("Message received (default): " + result.Message.Value);
+                    }
+                }
+            });
+
+            Task.Factory.StartNew(() =>
+            {
+                var config = new ConsumerConfig
+                {
+                    GroupId = "request-consumers",
+                    BootstrapServers = "kafka:29092",
+                    AutoOffsetReset = AutoOffsetReset.Earliest
+                };
+
+                var schemaRegistryUrl = "http://schema-registry:8085";
+                using (var schemaRegistry = new CachedSchemaRegistryClient(new SchemaRegistryConfig { Url = schemaRegistryUrl }))
+                using (var consumer = new ConsumerBuilder<string, GenericRecord>(config)
+                    .SetKeyDeserializer(new AvroDeserializer<string>(schemaRegistry).AsSyncOverAsync())
+                    .SetValueDeserializer(new AvroDeserializer<GenericRecord>(schemaRegistry).AsSyncOverAsync())
+                    .SetErrorHandler((_, e) => Console.WriteLine($"Error: {e.Reason}"))
+                    .Build())
+                {
+                    consumer.Subscribe("api-methods-requested-avro");
+                    while (true)
+                    {
+                        var result = consumer.Consume();
+                        Console.WriteLine("Message received (avro): " + result.Message.Value);
+                    }
+                }
+            });
+
+            Console.ReadLine();
+            Console.WriteLine("Exiting");
+        }
+    }
+}
